88542cfcb0ace88becb58b2cd41d6f769d1b5eeb
Joseph Thorne <joseph.thorne@student.uq.edu.au>
2026-02-24 12:11:32 +1000

    fixed prac 1 error

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -20,48 +20,48 @@ def get_todos():
 
 @api.route('/todos/<int:id>', methods=['GET'])
 def get_todo(id):
-    return jsonify([{
-        "id": id,
-        "title": "Watch CSSE6400 Lecture",
-        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-        "completed": True,
-        "deadline_at": "2026-02-27T18:00:00",
-        "created_at": "2026-02-20T14:00:00",
-        "updated_at": "2026-02-20T14:00:00",
-    }])
-
-@api.route('/todos', methods=['POST'])
-def create_todo():
-    return jsonify([{
+    return jsonify({
         "id": 1,
         "title": "Watch CSSE6400 Lecture",
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
         "completed": True,
         "deadline_at": "2026-02-27T18:00:00",
         "created_at": "2026-02-20T14:00:00",
-        "updated_at": "2026-02-20T14:00:00",
-    }])
+        "updated_at": "2026-02-20T14:00:00"
+    })
+
+@api.route('/todos', methods=['POST'])
+def create_todo():
+    return jsonify({
+        "id": 1,
+        "title": "Watch CSSE6400 Lecture",
+        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+        "completed": True,
+        "deadline_at": "2026-02-27T18:00:00",
+        "created_at": "2026-02-20T14:00:00",
+        "updated_at": "2026-02-20T14:00:00"
+    }), 201
 
 @api.route('/todos/<int:id>', methods=['PUT'])
 def update_todo(id):
-    return jsonify([{
-        "id": id,
+    return jsonify({
+        "id": 1,
         "title": "Watch CSSE6400 Lecture",
         "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
         "completed": True,
         "deadline_at": "2026-02-27T18:00:00",
         "created_at": "2026-02-20T14:00:00",
-        "updated_at": "2026-02-20T14:00:00",
-    }])
+        "updated_at": "2026-02-20T14:00:00"
+    })
 
 @api.route('/todos/<int:id>', methods=['DELETE'])
 def delete_todo(id):
-    return jsonify([{
-        "id": id,
-        "title": "Watch CSSE6400 Lecture",
-        "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
-        "completed": True,
-        "deadline_at": "2026-02-27T18:00:00",
-        "created_at": "2026-02-20T14:00:00",
-        "updated_at": "2026-02-20T14:00:00",
-    }])
+    return jsonify({
+            "id": 1,
+            "title": "Watch CSSE6400 Lecture",
+            "description": "Watch the CSSE6400 lecture on ECHO360 for week 1",
+            "completed": True,
+            "deadline_at": "2026-02-27T18:00:00",
+            "created_at": "2026-02-20T14:00:00",
+            "updated_at": "2026-02-20T14:00:00"
+    })
